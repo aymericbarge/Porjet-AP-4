8cad7d3141cc581e417c651199d2e7e9e1b7a723
hyrius-line <94972518+hyrius-line@users.noreply.github.com>
2024-03-13 13:37:10 +0100

    Ajout des vue article, commande et profil + ajout des activités article, commande et profil

---
+++ app/src/main/java/com/example/projetap3/ArticleActivity.java
@@ -0,0 +1,12 @@
+package com.example.projetap3;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ArticleActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.article_view);
+    }
+}
