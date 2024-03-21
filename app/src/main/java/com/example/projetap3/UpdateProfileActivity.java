58939632511aad256c41ae07c052834f7e4277e8
hyrius-line <94972518+hyrius-line@users.noreply.github.com>
2024-03-21 14:28:46 +0100

    test

---
+++ app/src/main/java/com/example/projetap3/UpdateProfileActivity.java
@@ -0,0 +1,12 @@
+package com.example.projetap3;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UpdateProfileActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.update_profile_view);
+    }
+}
